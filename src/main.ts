8ff551524e74d26bf6d250bb0f1b858986af9228
rb-mwindh <44254831+rb-mwindh@users.noreply.github.com>
2026-04-01 13:19:29 +0200

    feat(inputs): make bundle name optional with default value

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core';
 import {repo} from './lib/repo.js';
 
 (async () => {
-  const bundleName = core.getInput('bundle', {required: true});
+  const bundleName = core.getInput('bundle', {required: false}) || 'release';
   await repo.main(bundleName);
 })().catch((error: unknown) => {
   const message = error instanceof Error ? error.message : String(error);
--- src/pre.ts
+++ src/pre.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core';
 import {repo} from './lib/repo.js';
 
 (async () => {
-  const bundleName = core.getInput('bundle', {required: true});
+  const bundleName = core.getInput('bundle', {required: false}) || 'release';
   await repo.pre(bundleName);
 })().catch((error: unknown) => {
   const message = error instanceof Error ? error.message : String(error);
